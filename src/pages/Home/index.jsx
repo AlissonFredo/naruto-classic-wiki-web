a214d1c6ebf9364ca66919904d3af451498ff2ed
AlissonFredo <douglasalissonsf@gmail.com>
2025-02-24 23:10:17 -0300

    refactor: adjust Home page layout
    - Adds animations to the elements for a smoother user experience.
    
    - Introduces  to create an anchor for smooth scrolling.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,8 +3,18 @@ import NarutoUzumakiImg from "../../assets/naruto.uzumaki.jpg";
 import VillageSymbolKonohaImg from "../../assets/simbolo.aldeia.folha.png";
 import Header from "../../components/Header";
 import Container from "../../components/Container";
+import { Link } from "react-router";
+import { useRef } from "react";
 
 function Home() {
+  const exploreRef = useRef(null);
+
+  const scrollToExplore = () => {
+    if (exploreRef.current) {
+      exploreRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <Container>
       <Header />
@@ -17,42 +27,49 @@ function Home() {
           <h1 className="text-4xl font-bold">
             Discover the story of Classic Naruto
           </h1>
-          <a
-            href="#explore"
-            className="mt-4 bg-orange-500 hover:bg-orange-600 text-white text-2xl px-6 py-2 rounded-lg font-semibold transition"
+          <button
+            onClick={scrollToExplore}
+            className="mt-6 bg-orange-500 hover:bg-orange-600 text-white text-3xl px-6 py-2 rounded-lg font-semibold transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100"
           >
             Explore
-          </a>
+          </button>
         </div>
       </div>
 
       <div
-        className="h-full text-white flex flex-col items-center justify-center bg-radial-[at_50%_75%] from-sky-200 via-blue-400 to-indigo-900 to-90%"
+        className="h-full text-white flex flex-col items-center justify-center bg-radial-[at_50%_75%] from-orange-500 via-orange-400 to-orange-400 to-90%"
         id="explore"
+        ref={exploreRef}
       >
         <h2 className="text-4xl font-bold mb-10">
           Choose what you want to dive in:
         </h2>
-        <div className="grid grid-cols-2 gap-10">
-          <div>
-            <h3 className="mb-5 text-center text-3xl font-bold uppercase">
-              Characters
-            </h3>
-            <div>
-              <img
-                className="w-60 h-108 rounded-lg"
-                src={NarutoUzumakiImg}
-                alt=""
-              />
-            </div>
+
+        <div className="grid grid-cols-2 gap-x-15">
+          <div className="transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
+            <Link to="/characters">
+              <div
+                className="size-60 rounded-full bg-cover bg-center"
+                style={{ backgroundImage: `url(${NarutoUzumakiImg})` }}
+              ></div>
+              <h2 className="mt-8 rounded text-center text-white font-bold uppercase text-3xl">
+                Characters
+              </h2>
+            </Link>
           </div>
-          <div>
-            <h3 className="mb-5 text-center text-3xl font-bold uppercase">
-              Villages
-            </h3>
-            <div className="flex items-center justify-center w-60 h-108 rounded-lg bg-radial-[at_50%_85%] from-green-500 via-green-700 to-green-900 to-90%">
-              <img className="w-50" src={VillageSymbolKonohaImg} alt="" />
-            </div>
+          <div className="transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
+            <Link to="/">
+              <div
+                className="size-60 bg-white rounded-full bg-cover bg-center bg-no-repeat"
+                style={{
+                  backgroundImage: `url(${VillageSymbolKonohaImg})`,
+                  backgroundSize: "170px",
+                }}
+              ></div>
+              <h2 className="mt-8 rounded text-center text-white font-bold uppercase text-3xl">
+                Villages
+              </h2>
+            </Link>
           </div>
         </div>
       </div>
