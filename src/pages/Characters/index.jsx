cdf16b0262f1a1c091381df4143ce86d9875a069
AlissonFredo <douglasalissonsf@gmail.com>
2025-02-26 22:48:40 -0300

    feat: add functionality to fetch characters from the API
    - Implements a new feature to fetch all characters from the API.
    
    - Allows retrieving character data for use in the frontend.

--- src/pages/Characters/index.jsx
+++ src/pages/Characters/index.jsx
@@ -1,16 +1,35 @@
 import Container from "../../components/Container";
 import Header from "../../components/Header";
-import NarutoUzumakiImg from "../../assets/naruto.uzumaki.jpg";
+// import NarutoUzumakiImg from "../../assets/naruto.uzumaki.jpg";
 import { Link } from "react-router";
+import { api } from "../../services/api";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Characters() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
+  const getCharacters = async () => {
+    try {
+      const { data } = await api.get("/characters");
+      setCharacters(data);
+    } catch (error) {
+      if (!axios.isAxiosError(error)) return;
+      console.log(error);
+    }
+  };
+
   return (
     <Container>
       <Header />
       <div
         className="
           pt-25 
-          h-full
+          min-h-screen
           bg-radial-[at_50%_75%] from-orange-500 via-orange-400 to-orange-400 to-90%
         "
       >
@@ -29,26 +48,31 @@ function Characters() {
         </h1>
         <div className="flex justify-center">
           <div className="grid grid-cols-3 gap-x-50 gap-y-20">
-            {Array.from({ length: 3 }).map((_, key) => (
-              <div key={key} className="animate-pulse">
-                <div className="size-50 rounded-full bg-gray-200"></div>
-                <div className="mt-8 h-4 rounded bg-gray-200"></div>
-              </div>
-            ))}
+            {characters.length == 0 &&
+              Array.from({ length: 6 }).map((_, key) => (
+                <div key={key} className="animate-pulse">
+                  <div className="size-50 rounded-full bg-gray-200"></div>
+                  <div className="mt-8 h-4 rounded bg-gray-200"></div>
+                </div>
+              ))}
 
-            {Array.from({ length: 3 }).map((_, key) => (
-              <div key={key} className="transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
-                <Link to="/">
-                  <div
-                    className="size-50 rounded-full bg-cover bg-center"
-                    style={{ backgroundImage: `url(${NarutoUzumakiImg})` }}
-                  ></div>
-                  <h2 className="mt-8 rounded text-center text-white font-bold uppercase">
-                    Uzumaki Naruto
-                  </h2>
-                </Link>
-              </div>
-            ))}
+            {characters.length != 0 &&
+              characters.map((character, key) => (
+                <div
+                  key={key}
+                  className="transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
+                >
+                  <Link to="/">
+                    <div
+                      className="size-50 rounded-full bg-cover bg-center"
+                      style={{ backgroundImage: `url(${character.url})` }}
+                    ></div>
+                    <h2 className="mt-8 rounded text-center text-white font-bold uppercase">
+                      {character.name}
+                    </h2>
+                  </Link>
+                </div>
+              ))}
           </div>
         </div>
       </div>
