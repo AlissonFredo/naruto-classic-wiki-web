ab096845245fa0720d8e61df5f4c5162ed35e800
AlissonFredo <douglasalissonsf@gmail.com>
2025-03-03 15:35:57 -0300

    refactor: add generic listing component
    - Adds a component that will be used to list both characters and villages.

---
+++ src/components/List/index.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router";
+
+const List = ({ items = [], baseUrl = "" }) => {
+  return (
+    <div className="flex justify-center">
+      <div className="grid grid-cols-3 gap-x-50 gap-y-20">
+        {items.length == 0 &&
+          Array.from({ length: 6 }).map((_, key) => (
+            <div key={key} className="animate-pulse">
+              <div className="size-50 rounded-full bg-gray-200"></div>
+              <div className="mt-8 h-4 rounded bg-gray-200"></div>
+            </div>
+          ))}
+
+        {items.length != 0 &&
+          items.map((item, key) => (
+            <div
+              key={key}
+              className="transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
+            >
+              <Link to={`${baseUrl}/${item.id}`}>
+                <div
+                  className="size-50 rounded-full bg-cover bg-center"
+                  style={{ backgroundImage: `url(${item.url})` }}
+                ></div>
+                <h2 className="mt-8 rounded text-center text-white font-bold uppercase">
+                  {item.name}
+                </h2>
+              </Link>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default List;
--- src/pages/Characters/index.jsx
+++ src/pages/Characters/index.jsx
@@ -1,11 +1,11 @@
 import Container from "../../components/Container";
 import Header from "../../components/Header";
-import { Link } from "react-router";
 import { api } from "../../services/api";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Footer from "../../components/Footer";
 import Pagination from "../../components/Pagination";
+import List from "../../components/List";
 
 function Characters() {
   const [characters, setCharacters] = useState([]);
@@ -16,13 +16,9 @@ function Characters() {
     totalRecords: 0,
   });
 
-  const handlePagination = (page) => {
-    setPagination((prev) => ({ ...prev, currentPage: page }));
-  };
-
   useEffect(() => {
     getCharacters();
-  }, [pagination.currentPage]);
+  }, []);
 
   const getCharacters = async () => {
     try {
@@ -41,6 +37,10 @@ function Characters() {
       if (!axios.isAxiosError(error)) return;
       console.log(error);
     }
+  };
+
+  const handlePagination = (page) => {
+    setPagination((prev) => ({ ...prev, currentPage: page }));
   };
 
   return (
@@ -70,35 +70,8 @@ function Characters() {
         >
           Characters
         </h1>
-        <div className="flex justify-center">
-          <div className="grid grid-cols-3 gap-x-50 gap-y-20">
-            {characters.length == 0 &&
-              Array.from({ length: 6 }).map((_, key) => (
-                <div key={key} className="animate-pulse">
-                  <div className="size-50 rounded-full bg-gray-200"></div>
-                  <div className="mt-8 h-4 rounded bg-gray-200"></div>
-                </div>
-              ))}
 
-            {characters.length != 0 &&
-              characters.map((character, key) => (
-                <div
-                  key={key}
-                  className="transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
-                >
-                  <Link to={`/characters/${character.id}`}>
-                    <div
-                      className="size-50 rounded-full bg-cover bg-center"
-                      style={{ backgroundImage: `url(${character.url})` }}
-                    ></div>
-                    <h2 className="mt-8 rounded text-center text-white font-bold uppercase">
-                      {character.name}
-                    </h2>
-                  </Link>
-                </div>
-              ))}
-          </div>
-        </div>
+        <List items={characters} baseUrl="/characters" />
 
         <Pagination
           currentPage={pagination.currentPage}
