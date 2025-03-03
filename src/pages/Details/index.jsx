56f73116c50c492129e08724f21b3595697c55e9
AlissonFredo <douglasalissonsf@gmail.com>
2025-03-02 23:03:54 -0300

    feat: add character details fetching
    Fetch character by ID on the character details page to populate the screen with character data.

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,30 @@
 import Container from "../../components/Container";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import NarutoUzumakiImg from "../../assets/naruto.uzumaki.jpg";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 
 const Details = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
+
+  const [character, setCharacter] = useState({});
+
+  useEffect(() => {
+    getCharacter();
+  }, []);
+
+  const getCharacter = async () => {
+    try {
+      const { data } = await api.get(`/characters/${id}`);
+
+      setCharacter(data);
+    } catch (error) {
+      if (!axios.isAxiosError(error)) return;
+      console.log(error);
+    }
+  };
 
   return (
     <Container>
@@ -25,20 +44,41 @@ const Details = () => {
             <div className="flex justify-center">
               <div
                 className="size-60 rounded-full bg-cover bg-center"
-                style={{ backgroundImage: `url(${NarutoUzumakiImg})` }}
+                style={{ backgroundImage: `url(${character.url})` }}
               ></div>
             </div>
             <div className="flex flex-col justify-center">
               <h1 className="text-start text-white font-bold uppercase text-4xl">
-                Naruto Uzumaki
+                {character.name}
               </h1>
             </div>
             <div className="col-span-2 px-20 py-10 mt-8 text-white font-bold uppercase bg-stone-800/10">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
-              ea, quas rem dolorem quibusdam blanditiis, sit pariatur nemo
-              corrupti quae illo nisi minima harum ad iste assumenda explicabo
-              magni ipsam!
+              {character.about}
             </div>
+          </div>
+          <div className="text-end py-5">
+            <button
+              className="
+                text-white 
+                font-bold 
+                uppercase 
+                px-4 
+                py-2 
+                rounded-lg 
+                bg-blue-500 
+                transition 
+                delay-150 
+                duration-300 
+                ease-in-out 
+                hover:-translate-y-1 
+                hover:scale-100 
+                hover:bg-blue-500
+                hover:opacity-75
+              "
+              onClick={() => navigate(-1)}
+            >
+              go Back
+            </button>
           </div>
         </div>
       </div>
